b406a0e9492d8a5c72f3a2b6a98a5deec7d256b0
Courtney Carson <cmacarson@gmail.com>
2023-10-30 19:29:30 -0700

    globalcss

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,85 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <Link href="/">Hello </Link>
+      <Link href="/">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+          similique fuga labore officia sequi, dolore consequatur pariatur,
+          dolorem quidem quas illum accusamus numquam incidunt asperiores
+          aliquid nemo repellendus, dolorum deleniti.
+        </h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum numquam
+        dolores mollitia dolore voluptatum labore provident adipisci maxime quas
+        reprehenderit. A dolore molestias assumenda in veritatis esse rerum
+        corrupti corporis.lorem Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Amet iusto quam voluptatum maiores deserunt magnam
+        minima similique repellendus beatae. Repellat est perspiciatis
+        asperiores velit voluptate illum molestias, provident reiciendis
+        exercitationem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Illum numquam dolores mollitia dolore voluptatum labore provident
+        adipisci maxime quas reprehenderit. A dolore molestias assumenda in
+        veritatis esse rerum corrupti corporis.lorem Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Amet iusto quam voluptatum maiores
+        deserunt magnam minima similique repellendus beatae. Repellat est
+        perspiciatis asperiores velit voluptate illum molestias, provident
+        reiciendis exercitationem? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Illum numquam dolores mollitia dolore voluptatum
+        labore provident adipisci maxime quas reprehenderit. A dolore molestias
+        assumenda in veritatis esse rerum corrupti corporis.lorem Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Amet iusto quam voluptatum
+        maiores deserunt magnam minima similique repellendus beatae. Repellat
+        est perspiciatis asperiores velit voluptate illum molestias, provident
+        reiciendis exercitationem? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Illum numquam dolores mollitia dolore voluptatum
+        labore provident adipisci maxime quas reprehenderit. A dolore molestias
+        assumenda in veritatis esse rerum corrupti corporis.lorem Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Amet iusto quam voluptatum
+        maiores deserunt magnam minima similique repellendus beatae. Repellat
+        est perspiciatis asperiores velit voluptate illum molestias, provident
+        reiciendis exercitationem? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Illum numquam dolores mollitia dolore voluptatum
+        labore provident adipisci maxime quas reprehenderit. A dolore molestias
+        assumenda in veritatis esse rerum corrupti corporis.lorem Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Amet iusto quam voluptatum
+        maiores deserunt magnam minima similique repellendus beatae. Repellat
+        est perspiciatis asperiores velit voluptate illum molestias, provident
+        reiciendis exercitationem? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Illum numquam dolores mollitia dolore voluptatum
+        labore provident adipisci maxime quas reprehenderit. A dolore molestias
+        assumenda in veritatis esse rerum corrupti corporis.lorem Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Amet iusto quam voluptatum
+        maiores deserunt magnam minima similique repellendus beatae. Repellat
+        est perspiciatis asperiores velit voluptate illum molestias, provident
+        reiciendis exercitationem? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Illum numquam dolores mollitia dolore voluptatum
+        labore provident adipisci maxime quas reprehenderit. A dolore molestias
+        assumenda in veritatis esse rerum corrupti corporis.lorem Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Amet iusto quam voluptatum
+        maiores deserunt magnam minima similique repellendus beatae. Repellat
+        est perspiciatis asperiores velit voluptate illum molestias, provident
+        reiciendis exercitationem? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Illum numquam dolores mollitia dolore voluptatum
+        labore provident adipisci maxime quas reprehenderit. A dolore molestias
+        assumenda in veritatis esse rerum corrupti corporis.lorem Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Amet iusto quam voluptatum
+        maiores deserunt magnam minima similique repellendus beatae. Repellat
+        est perspiciatis asperiores velit voluptate illum molestias, provident
+        reiciendis exercitationem? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Illum numquam dolores mollitia dolore voluptatum
+        labore provident adipisci maxime quas reprehenderit. A dolore molestias
+        assumenda in veritatis esse rerum corrupti corporis.lorem Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Amet iusto quam voluptatum
+        maiores deserunt magnam minima similique repellendus beatae. Repellat
+        est perspiciatis asperiores velit voluptate illum molestias, provident
+        reiciendis exercitationem? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Illum numquam dolores mollitia dolore voluptatum
+        labore provident adipisci maxime quas reprehenderit. A dolore molestias
+        assumenda in veritatis esse rerum corrupti corporis.lorem Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Amet iusto quam voluptatum
+        maiores deserunt magnam minima similique repellendus beatae. Repellat
+        est perspiciatis asperiores velit voluptate illum molestias, provident
+        reiciendis exercitationem?
+      </Link>
     </div>
   );
 }
