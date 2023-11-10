54a91c52f8c217de4df8bae0fb7eb0f0a420c8ce
Courtney Carson <courtneycarson@Courtneys-Mac-mini.local>
2023-11-09 18:17:05 -0800

    featured hero section of blog post

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import styles from './featured.module.css';
+import Image from 'next/image';
 
 export const Featured = () => {
-  return <div className={styles.container}>featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Blog User Here!</b>
+        Check out my posts and ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet alim consectetur adipisicing elit.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Cupiditate, quam nisi magni ea laborum inventore voluptatum
+            laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium
+            quisquam! Harum unde sit culpa debitis.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
