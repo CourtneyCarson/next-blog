23efa233f453b278ae30e8ca737b9f5c5238396c
Your NameCourtney <cmacarson@gmail.com>
2023-11-09 18:38:25 -0800

    starting category list

--- src/components/categoryList/categoryList.jsx
+++ src/components/categoryList/categoryList.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import styles from './categorylist.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export const CategoryList = () => {
-  return <div className={styles.container}>Category List</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.category}>Popular Categories</h1>
+      <div className={styles.category}>
+        <div className={styles.category}>
+          <Link href='/blog?cat=style'>
+            <Image
+              src="/style.png"
+              alt=""
+              width={32}
+              height={32}
+              className={styles.image}
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
