ffc8eee5a342ea23540aec9d4c82f91ab829f1d6
Your NameCourtney <cmacarson@gmail.com>
2023-11-16 17:30:37 -0800

    next auth set up, google oath works

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
-import { Navbar } from '@/components/navbar/Navbar';
-import './globals.css';
-import { Inter } from 'next/font/google';
 import { Footer } from '@/components/footer/Footer';
+import { Navbar } from '@/components/navbar/Navbar';
 import { ThemeContextProvider } from '@/context/ThemeContext';
+
 import { ThemeProvider } from '@/providers/ThemeProvider';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import AuthProvider from '@/providers/authProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,17 +18,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeContextProvider>
-          <ThemeProvider>
-            <div className="container">
-              <div className="wrapper">
-                <Navbar />
-                {children}
-                <Footer />
+        <AuthProvider >
+          <ThemeContextProvider>
+            <ThemeProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Navbar />
+                  {children}
+                  <Footer />
+                </div>
               </div>
-            </div>
-          </ThemeProvider>
-        </ThemeContextProvider>
+            </ThemeProvider>
+          </ThemeContextProvider>
+        </AuthProvider>
       </body>
     </html>
   );
