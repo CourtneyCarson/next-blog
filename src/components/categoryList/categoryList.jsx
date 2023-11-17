bbd8d87300ac00626f9efa028b0006201546380f
Your NameCourtney <cmacarson@gmail.com>
2023-11-16 19:05:20 -0800

    prisma schema hydrated, map categories for category list

--- src/components/categoryList/categoryList.jsx
+++ src/components/categoryList/categoryList.jsx
@@ -1,92 +1,42 @@
-import React from 'react';
-import styles from './categorylist.module.css';
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
+import styles from './categorylist.module.css';
 
-export const CategoryList = () => {
+const getData = async () => {
+  const res = await fetch('http://localhost:3000/api/categories', {
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+  return res.json();
+};
+
+export const CategoryList = async () => {
+  const data = await getData();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       {/* add both cat and style classes */}
       <div className={styles.categories}>
-
-        <Link
-          href="/blog"
-          className={`${styles.category} ${styles.style}`}>
-          <Image
-            src="/style.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          style
-        </Link>
-        {/* travel */}
-        <Link
-          href="/blog?cat=travel"
-          className={`${styles.category} ${styles.travel}`}>
-          <Image
-            src="/travel.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          travel
-        </Link>
-        {/* culture */}
-        <Link
-          href="/blog?cat=culture"
-          className={`${styles.category} ${styles.culture}`}>
-          <Image
-            src="/culture.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          culture
-        </Link>
-        {/* coding */}
-        <Link
-          href="/blog?cat=coding"
-          className={`${styles.category} ${styles.coding}`}>
-          <Image
-            src="/coding.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          coding
-        </Link>
-        {/* food */}
-        <Link
-          href="/blog?cat=food"
-          className={`${styles.category} ${styles.food}`}>
-          <Image
-            src="/food.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          food
-        </Link>
-        {/* fashion */}
-        <Link
-          href="/blog?cat=fashion"
-          className={`${styles.category} ${styles.fashion}`}>
-          <Image
-            src="/fashion.png"
-            alt=""
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          fashion
-        </Link>
+        {data.map((item) => (
+          <Link
+            href="/blog"
+            className={`${styles.category} ${styles[item.slug]}`}
+            key={item._id}>
+            {item.img && (
+              <Image
+                src={item.img}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            )}
+            {item.title}
+          </Link>
+        ))}
       </div>
     </div>
   );
